194f1663141418ec0dd5d3c3b3911147ec65c79b
hunaisong <hunaisong@longfor.com>
2021-06-08 19:19:40 +0800

    初始化项目，完成docker页面

---
+++ src/views/Docker.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="docker">
+    <div
+      :class="[
+        'docker__item',
+        index === activeIndex ? 'docker__item--active' : '',
+      ]"
+      v-for="(item, index) in dockerList"
+      :key="index"
+      @click="changeTag(index)"
+    >
+      <div class="iconfont docker__item__font" v-html="item.icon"></div>
+      <div class="docker__item__desc">{{ item.label }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+export default defineComponent({
+  name: "Docker",
+  setup() {
+    let state = reactive({
+      activeIndex: 0,
+    });
+    const changeTag = (index: number): void => {
+      state.activeIndex = index;
+    };
+    const { activeIndex } = toRefs(state);
+    return {
+      dockerList: [
+        { icon: "&#xe89c;", label: "首页" },
+        { icon: "&#xe6fc;", label: "购物车" },
+        { icon: "&#xe643;", label: "订单" },
+        { icon: "&#xe601;", label: "我的" },
+      ],
+      activeIndex,
+      changeTag,
+    };
+  },
+});
+</script>
+
+<style lang="less" scoped>
+@import url("../style/variable.less");
+.docker {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 0.49rem;
+  padding: 0.06rem 0.16rem;
+  box-sizing: border-box;
+  display: flex;
+  text-align: center;
+  border-top: 0.01rem solid #f1f1f1;
+  &__item {
+    flex: 1;
+    &__font {
+      font-size: 0.2rem;
+      color: @content-color;
+      line-height: 0.2rem;
+    }
+    &__desc {
+      font-size: 0.2rem;
+      line-height: 0.2rem;
+      transform: scale(0.5);
+      transform-origin: top center;
+      color: #333;
+      margin-top: 0.03rem;
+    }
+  }
+  &__item--active {
+    .docker__item__font,
+    .docker__item__desc {
+      color: @content-highlight-color;
+    }
+  }
+}
+</style>
