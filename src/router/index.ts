d331138708fc98e5e53f7abdfa1d416159b1a50c
hunaisong <hunaisong@longfor.com>
2021-06-16 19:10:13 +0800

    注册页面完成

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home/Index.vue";
 import Login from "../views/Login/Index.vue";
+import Register from "../views/Register/Index.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -17,6 +18,15 @@ const routes: Array<RouteRecordRaw> = [
       isLogin ? next({ name: "Home" }) : next();
     },
   },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+    beforeEnter(to, from, next) {
+      const { isLogin } = localStorage;
+      isLogin ? next({ name: "Home" }) : next();
+    },
+  },
 ];
 
 const router = createRouter({
@@ -26,7 +36,9 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const { isLogin } = localStorage;
-  isLogin || to.name === "Login" ? next() : next({ name: "Login" });
+  const isLoginPageOrRegisterPage =
+    to.name === "Login" || to.name === "Register";
+  isLogin || isLoginPageOrRegisterPage ? next() : next({ name: "Login" });
 });
 
 export default router;
---
+++ src/views/Register/Index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="register">
+    <div class="register__img">
+      <span class="iconfont">&#xe607;</span>
+    </div>
+    <div class="register__username">
+      <input type="text" placeholder="请输入手机号" />
+    </div>
+    <div class="register__password">
+      <input type="password" placeholder="请输入密码" />
+    </div>
+    <div class="register__confirmPassword">
+      <input type="password" placeholder="确认密码" />
+    </div>
+    <div class="register__button">
+      <button @click="handleClick">注 册</button>
+    </div>
+    <p class="register__register">已有账号去登录</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+export default defineComponent({
+  name: "Register",
+  setup() {
+    const router = useRouter();
+    const handleClick = () => {
+      localStorage.isLogin = true;
+      router.push({ name: "Home" });
+    };
+    return {
+      handleClick,
+    };
+  },
+});
+</script>
+
+<style lang="less" scoped>
+@import url("../../style/mixin.less");
+.register {
+  font-size: 0.12rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: #fff;
+  padding: 0 0.4rem;
+  &__img {
+    margin-top: 1.2rem;
+    margin-bottom: 0.4rem;
+    text-align: center;
+    span {
+      color: #0091ff;
+      font-size: 0.66rem;
+    }
+  }
+  &__username {
+    margin-bottom: 0.16rem;
+    input {
+      .login-input();
+    }
+  }
+  &__password {
+    margin-bottom: 0.16rem;
+    input {
+      .login-input();
+    }
+  }
+  &__confirmPassword {
+    margin-bottom: 0.32rem;
+    input {
+      .login-input();
+    }
+  }
+  &__button {
+    margin-bottom: 0.16rem;
+    button {
+      width: 2.95rem;
+      height: 0.48rem;
+      box-sizing: border-box;
+      background: #0091ff;
+      color: #fff;
+      border-radius: 0.04rem;
+      font-size: 0.16rem;
+      padding: 0.12rem 0;
+      border: 0;
+    }
+  }
+  &__register {
+    font-size: 0.16rem;
+    text-align: center;
+  }
+}
+</style>
