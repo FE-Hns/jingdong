f9260489730f6dc12d13b07638f28360147d8ba7
hunaisong <hunaisong@longfor.com>
2021-06-28 15:42:01 +0800

    将商家布局提出组件

---
+++ src/components/Shop/ShopItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="shop">
+    <div class="shop__item">
+      <div class="shop__item__img">
+        <img :src="data.imgUrl" alt="" />
+      </div>
+      <div class="shop__item__desc">
+        <p class="shop__item__desc__title">{{ data.name }}</p>
+        <p class="shop__item__desc__detail">
+          <span>月售 {{ data.sales }}</span>
+          <span>起送￥{{ data.expressExpensive }}</span>
+          <span>基础运费￥{{ data.basicExpensive }}</span>
+        </p>
+        <p class="shop__item__desc__discount">
+          {{ data.description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "ShopItem",
+  props: ["data"],
+});
+</script>
+
+<style lang="less" scoped>
+.shop {
+  font-size: 0.12rem;
+  &__item {
+    display: flex;
+    margin-bottom: 0.12rem;
+    &__img {
+      width: 0.56rem;
+      height: 0.56rem;
+      margin-right: 0.16rem;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    &__desc {
+      flex: 1;
+      border-bottom: 1px solid #f1f1f1;
+      &__title {
+        margin: 0 0 0.08rem 0;
+        font-size: 0.16rem;
+        color: #333;
+      }
+      &__detail {
+        margin: 0 0 0.08rem 0;
+        font-size: 0.13rem;
+        color: #333;
+        span {
+          margin-right: 0.08rem;
+        }
+      }
+      &__discount {
+        font-size: 0.13rem;
+        color: #e93b3b;
+        margin: 0 0 0.12rem 0;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home/NearBy/Index.vue
+++ src/views/Home/NearBy/Index.vue
@@ -1,24 +1,7 @@
 <template>
   <div class="nearby">
     <h3 class="nearby__title">附近店铺</h3>
-    <div class="nearby__content">
-      <div class="nearby__content__item" v-for="item in list" :key="item.id">
-        <div class="nearby__content__item__img">
-          <img :src="item.imgUrl" alt="" />
-        </div>
-        <div class="nearby__content__item__desc">
-          <p class="nearby__content__item__desc__title">{{ item.name }}</p>
-          <p class="nearby__content__item__desc__detail">
-            <span>月售 {{ item.sales }}</span>
-            <span>起送￥{{ item.expressExpensive }}</span>
-            <span>基础运费￥{{ item.basicExpensive }}</span>
-          </p>
-          <p class="nearby__content__item__desc__discount">
-            {{ item.description }}
-          </p>
-        </div>
-      </div>
-    </div>
+    <ShopItem v-for="item in list" :key="item.id" :data="item" />
   </div>
 </template>
 
@@ -26,6 +9,7 @@
 import { defineComponent, reactive, onMounted, toRefs } from "vue";
 import { get } from "../../../utils/request";
 import api from "../../../api/Index";
+import ShopItem from "../../../components/Shop/ShopItem.vue";
 interface listType {
   id: number | string;
   basicExpensive: number;
@@ -37,6 +21,7 @@ interface listType {
 }
 export default defineComponent({
   name: "nearby",
+  components: { ShopItem },
   setup() {
     let data = reactive<{ list: listType[] }>({
       list: [],
@@ -70,44 +55,6 @@ export default defineComponent({
     color: #333;
     font-weight: normal;
     margin: 0 0 0.12rem 0;
-  }
-  &__content {
-    font-size: 0.12rem;
-    &__item {
-      display: flex;
-      margin-bottom: 0.12rem;
-      &__img {
-        width: 0.56rem;
-        height: 0.56rem;
-        margin-right: 0.16rem;
-        img {
-          width: 100%;
-          height: 100%;
-        }
-      }
-      &__desc {
-        flex: 1;
-        border-bottom: 1px solid #f1f1f1;
-        &__title {
-          margin: 0 0 0.08rem 0;
-          font-size: 0.16rem;
-          color: #333;
-        }
-        &__detail {
-          margin: 0 0 0.08rem 0;
-          font-size: 0.13rem;
-          color: #333;
-          span {
-            margin-right: 0.08rem;
-          }
-        }
-        &__discount {
-          font-size: 0.13rem;
-          color: #e93b3b;
-          margin: 0 0 0.12rem 0;
-        }
-      }
-    }
   }
 }
 </style>
